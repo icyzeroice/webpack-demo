7b350d5c85c65d685b03c1d5078cf68b2c2b51bd
Ice Zero <ziv3@outlook.com>
2017-07-23 11:50:47 +0800

    Using webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function component() {
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    btn.innerHTML = 'Click me and check the console!';
+    btn.innerHTML = 'Click me and check the console! (webpack-dev-server)';
     btn.onclick = printMe;
 
     element.appendChild(btn);
