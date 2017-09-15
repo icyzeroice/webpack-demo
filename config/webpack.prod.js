0dddaa70d0d0830f4caf84da6dfbfb0f522d80dc
Ice Zero <ziv3@outlook.com>
2017-09-15 20:39:10 +0800

    [feat]: uglify;

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,11 @@
+
+// NEXT: Uglify
+
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const commonConfig = require('./webpack.common');
 
 let prodConfig = merge(commonConfig, {
@@ -13,6 +17,19 @@ let prodConfig = merge(commonConfig, {
   plugins: [
     new CleanWebpackPlugin('./dist', {
       root: path.resolve(__dirname, '../')
+    }),
+
+    // output uglified source
+    new UglifyjsWebpackPlugin({
+      test: /\.jsx?/i,
+      parallel: true,
+      uglifyOptions: {
+        ecma: 5,
+        output: {
+          comments: false,
+          beautiful: false
+        }
+      }
     })
   ]
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+/!* Ice Zero */
 import NavBar from './components/nav-bar/nav-bar';
 import Stage from './components/stage/stage'
 
